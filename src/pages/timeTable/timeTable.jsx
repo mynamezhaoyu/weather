c85b654fae851a97165a67dc508da122aac77eff
zhaoyu <13370229059@163.com>
2020-02-27 11:18:51 +0800

    feat: 添加F7前的代码提交实验

--- src/pages/timeTable/timeTable.jsx
+++ src/pages/timeTable/timeTable.jsx
@@ -1,16 +1,23 @@
 import Taro, { useState, useEffect } from '@tarojs/taro';
-import { View, Text } from '@tarojs/components';
+import { View, Text, ScrollView } from '@tarojs/components';
+import IconFont from '../../components/iconfont';
 import './timeTable.scss';
 function TimeTable() {
-  const [arr, setArr] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
+  const [arr, setArr] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
   return (
-    <View className="time-table">
-      <View className="child">
-        {arr.map((r) => {
-          return <View className='item' style={`width: ${100 / arr.length}%`}>{r}</View>;
-        })}
-      </View>
-    </View>
+    <ScrollView className="scroll" scrollX="true" scrollAnchoring="true">
+      {arr.map((r, i) => {
+        return (
+          <View className="list">
+            <View>{r}</View>
+            <View>
+              <IconFont name="qingtian" size="40" />
+            </View>
+            <View>{i}</View>
+          </View>
+        );
+      })}
+    </ScrollView>
   );
 }
 export default TimeTable;
